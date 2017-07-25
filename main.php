46ccd7a7c8ab112e433a60db79f8ddff642265d4
Romans Malinovskis <me@nearly.guru>
2017-07-25 16:42:33 +0100

    clean up old confusing coment.

--- main.php
+++ main.php
@@ -7,28 +7,6 @@ use \atk4\ui\Header;
 $app = new \atk4\ui\App('Computer will guess the number');
 $app->initLayout('Centered');
 
-/*if (!isset($_GET['r'])) {
-  $b = 100;
-  $m = 1;
-  $n =round(($b + $m) / 2)-1;
-  $app->add(new Header(['Your number is '.$n.' ?', 'size'=>1]));
-} else {
-    $n = $_GET['n'];
-    $m = $_GET['m'];
-    $b = $_GET['b'];
-          if ($_GET['r'] == 'w') {
-            $app->add(new Header(['Your number is '.$n.' !', 'size'=>1]));
-          } elseif ($_GET['r'] == 's') {
-            $b = $n;
-            $n = round (($b + $m) / 2);
-            $app->add(new Header(['Your number is '.$n.' ?', 'size'=>1]));
-          } elseif ($_GET['r'] == 'b') {
-            $m = $n;
-            $n = round (($b + $m) / 2);
-            $app->add(new Header(['Your number is '.$n.' ?', 'size'=>1]));
-          }
-      } */
-
     $b = $_GET['b'];
     $m = $_GET['m'];
 
